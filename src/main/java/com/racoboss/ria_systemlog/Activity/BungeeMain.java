7e1b87c7ccb7b211487dd4bb2a2cd4eef2a12f96
RaoBoss <cjykmk0689@naver.com>
2024-12-03 10:06:58 +0900

    TEST

---
+++ src/main/java/com/racoboss/ria_systemlog/Activity/BungeeMain.java
@@ -0,0 +1,14 @@
+package com.racoboss.ria_systemlog.Activity;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeMain extends Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
